15990b3238438f9136becef048bd32015e9bfd78
devmobile <antoniojoseuchoa2023@gmai.com>
2023-07-11 11:13:50 -0300

    Commit implementando MVVM com dependencias e novas formas de chamar o ViewModel

--- app/src/main/java/com/example/usandodatabinding/view/MainActivity.kt
+++ app/src/main/java/com/example/usandodatabinding/view/MainActivity.kt
@@ -2,26 +2,31 @@ package com.example.usandodatabinding.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.usandodatabinding.databinding.ActivityMainBinding
 import com.example.usandodatabinding.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private lateinit var mainViewModel: MainViewModel
+    //private val mainViewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        mainViewModel = defaultViewModelProviderFactory.create(MainViewModel::class.java)
+        //mainViewModel = defaultViewModelProviderFactory.create(MainViewModel::class.java)
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
         binding.btRecuperarTimes.setOnClickListener {
-            mainViewModel.getListTimes()
+             //mainViewModel.getListTimes2()
         }
 
     }
 
     override fun onStart() {
-        mainViewModel.time.observe(this){ times ->
+        mainViewModel.getListTimes2().observe(this){ times ->
             var resultado = ""
             times.forEach{
                  resultado += "$it \n"
--- app/src/main/java/com/example/usandodatabinding/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/usandodatabinding/viewmodel/MainViewModel.kt
@@ -3,17 +3,24 @@ package com.example.usandodatabinding.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
 import com.example.usandodatabinding.api.ServiceApi
 import com.example.usandodatabinding.model.Time
 
 class MainViewModel: ViewModel() {
 
-    private var _times = MutableLiveData<List<Time>>()
-    val time: LiveData<List<Time>> = _times
+//    private var _times = MutableLiveData<List<Time>>()
+//    val time: LiveData<List<Time>> = _times
+//
+//    fun getListTimes(){
+//        val list = ServiceApi.getListTimes()
+//        _times.value = list
+//    }
 
-    fun getListTimes(){
+    fun getListTimes2() = liveData{
         val list = ServiceApi.getListTimes()
-        _times.value = list
+        emit(list)
     }
+
 
 }
